4f0f54e8f69daa89244d7c442a0fc164572e6afe
Rehman <rehman.ali2012@gmail.com>
2025-04-09 14:38:34 +0300

    End of section 10

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,6 +8,8 @@ import { authGuard } from './_guards/auth.guard';
 import { TestErrosComponent } from './erros/test-erros/test-erros.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 export const routes: Routes = [
 
@@ -19,6 +21,7 @@ export const routes: Routes = [
         children: [
             { path: 'members', component: MemberListComponent, canActivate: [authGuard] },
             { path: 'members/:username', component: MemberDetailComponent },
+            { path: 'member/:edit', component: MemberEditComponent, canDeactivate:[preventUnsavedChangesGuard] },
             { path: '', component: HomeComponent },
             { path: 'lists', component: ListsComponent },
             { path: 'messages', component: MessagesComponent }
